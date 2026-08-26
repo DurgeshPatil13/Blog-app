d521eae2f62e4e1b9d3a1c40625f293b405eda82
DurgeshPatil13 <durgeshpatil0513@gmail.com>
2026-08-26 22:45:20 +0530

    starting

---
+++ index.js
@@ -0,0 +1,8 @@
+const express=require("express");
+
+const app=express();
+const PORT=8000;
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+app.listen(PORT,()=> console.log("started  "))
