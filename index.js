25e6fe3c948502ed10b4d3f5a295427c00b92a26
DurgeshPatil13 <durgeshpatil0513@gmail.com>
2026-08-28 22:17:11 +0530

    project setup

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express=require("express");
+const express = require("express");
+const app = express();
+const{  connectDB}=require("./connection.js")
+const PORT = 8001;
+require("dotenv").config();
+app.use(express.json());
+connectDB(process.env.MONGO_URL)
+app.get("/", (req, res) => {
+    res.json("Blog API is running");
+});
 
-const app=express();
-const PORT=8000;
-app.set("view engine","ejs");
-app.set("views",path.resolve("./views"));
-
-app.listen(PORT,()=> console.log("started  "))
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const userschema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+       type:String,
+    required:true,
+    unique:true
+    },
+    password:{
+            type:String,
+    required:true
+    }
+});
+const user=mongoose.model("blogapp",userschema)
+module.exports={
+    user
+}
